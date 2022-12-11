de0147a9f40d9ffb745169d370ba583f9300df50
Noel <noel.ruizlopez@capgemini.com>
2022-12-11 16:39:14 +0100

    Substituyendo MD5 por SHA256 con salt de 16bytes fijo

--- app/helpers/HashUtils.java
+++ app/helpers/HashUtils.java
@@ -3,6 +3,9 @@ package helpers;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 public class HashUtils {
 
@@ -22,4 +25,24 @@ public class HashUtils {
         } catch (Exception e) {}
         return s;
     }
+    public static String getSHA256Salt(String password) {
+
+        String generatedPassword = null;
+        try {
+            String str_salt = "hhJad2n7R33nbiiE2v8DNA==";
+            byte[] salt = Base64.getDecoder().decode(str_salt);
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(salt);
+            byte[] bytes = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
 }
